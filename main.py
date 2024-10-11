3be21673a0ad2605123514dc7fba44797f972d9b
el-gabry <engineergabry@gmail.com>
2024-10-11 06:16:55 -0400

    data ingestion with drive link

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from cnnClassifier import logger
+from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Starting the Chest Cancer Classification Project")
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
